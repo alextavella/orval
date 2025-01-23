d78afc325c4183bb1f29d3100734845836e585b3
alextavella <alex.tavella@azos.com.br>
2025-01-23 14:09:04 -0300

    refactor: use default value for db

--- packages/server/src/env.ts
+++ packages/server/src/env.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import { z } from 'zod'
 
 const envSchema = z.object({
-  DB_FILE_NAME: z.string(),
+  DB_FILE_NAME: z.string().default('file:test.db'),
 })
 
 export const env = envSchema.parse(process.env)
