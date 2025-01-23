2542d9c6ec33ace4ad61a6ff6274729bf5373125
alextavella <alex.tavella@azos.com.br>
2025-01-23 12:55:24 -0300

    build: config and commands

--- packages/client/src/app/data/appstore.ts
+++ packages/client/src/app/data/appstore.ts
@@ -5,87 +5,106 @@
  * Sample backend service
  * OpenAPI spec version: 1.0.0
  */
-import {
-  useMutation
-} from '@tanstack/react-query'
 import type {
   MutationFunction,
   UseMutationOptions,
-  UseMutationResult
+  UseMutationResult,
 } from '@tanstack/react-query'
-import type {
-  CreateNote201,
-  CreateNoteBody
-} from './model'
-import { customInstance } from '../lib/api';
-import type { ErrorType, BodyType } from '../lib/api';
+import { useMutation } from '@tanstack/react-query'
+import type { BodyType, ErrorType } from '../lib/api'
+import { customInstance } from '../lib/api'
+import type { CreateNote201, CreateNoteBody } from './model'
 
-
-type SecondParameter<T extends (...args: any) => any> = Parameters<T>[1];
-
+type SecondParameter<T extends (...args: any) => any> = Parameters<T>[1]
 
 /**
  * Create a note
  * @summary notes
  */
 export const createNote = (
-    createNoteBody: BodyType<CreateNoteBody>,
- options?: SecondParameter<typeof customInstance>,signal?: AbortSignal
+  createNoteBody: BodyType<CreateNoteBody>,
+  options?: SecondParameter<typeof customInstance>,
+  signal?: AbortSignal,
 ) => {
-      
-      
-      return customInstance<CreateNote201>(
-      {url: `/notes`, method: 'POST',
-      headers: {'Content-Type': 'application/json', },
-      data: createNoteBody, signal
+  return customInstance<CreateNote201>(
+    {
+      url: `/notes`,
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      data: createNoteBody,
+      signal,
     },
-      options);
-    }
-  
+    options,
+  )
+}
 
+export const getCreateNoteMutationOptions = <
+  TData = Awaited<ReturnType<typeof createNote>>,
+  TError = ErrorType<unknown>,
+  TContext = unknown,
+>(options?: {
+  mutation?: UseMutationOptions<
+    TData,
+    TError,
+    { data: BodyType<CreateNoteBody> },
+    TContext
+  >
+  request?: SecondParameter<typeof customInstance>
+}) => {
+  const mutationKey = ['createNote']
+  const { mutation: mutationOptions, request: requestOptions } = options
+    ? options.mutation &&
+      'mutationKey' in options.mutation &&
+      options.mutation.mutationKey
+      ? options
+      : { ...options, mutation: { ...options.mutation, mutationKey } }
+    : { mutation: { mutationKey }, request: undefined }
 
-export const getCreateNoteMutationOptions = <TData = Awaited<ReturnType<typeof createNote>>, TError = ErrorType<unknown>,
-    TContext = unknown>(options?: { mutation?:UseMutationOptions<TData, TError,{data: BodyType<CreateNoteBody>}, TContext>, request?: SecondParameter<typeof customInstance>}
-) => {
-const mutationKey = ['createNote'];
-const {mutation: mutationOptions, request: requestOptions} = options ?
-      options.mutation && 'mutationKey' in options.mutation && options.mutation.mutationKey ?
-      options
-      : {...options, mutation: {...options.mutation, mutationKey}}
-      : {mutation: { mutationKey, }, request: undefined};
+  const mutationFn: MutationFunction<
+    Awaited<ReturnType<typeof createNote>>,
+    { data: BodyType<CreateNoteBody> }
+  > = props => {
+    const { data } = props ?? {}
 
-      
+    return createNote(data, requestOptions)
+  }
 
+  return { mutationFn, ...mutationOptions } as UseMutationOptions<
+    TData,
+    TError,
+    { data: BodyType<CreateNoteBody> },
+    TContext
+  >
+}
 
-      const mutationFn: MutationFunction<Awaited<ReturnType<typeof createNote>>, {data: BodyType<CreateNoteBody>}> = (props) => {
-          const {data} = props ?? {};
+export type CreateNoteMutationResult = NonNullable<
+  Awaited<ReturnType<typeof createNote>>
+>
+export type CreateNoteMutationBody = BodyType<CreateNoteBody>
+export type CreateNoteMutationError = ErrorType<unknown>
 
-          return  createNote(data,requestOptions)
-        }
-
-        
-
-
-  return  { mutationFn, ...mutationOptions } as UseMutationOptions<TData, TError,{data: BodyType<CreateNoteBody>}, TContext>}
-
-    export type CreateNoteMutationResult = NonNullable<Awaited<ReturnType<typeof createNote>>>
-    export type CreateNoteMutationBody = BodyType<CreateNoteBody>
-    export type CreateNoteMutationError = ErrorType<unknown>
-
-    /**
+/**
  * @summary notes
  */
-export const useCreateNote = <TData = Awaited<ReturnType<typeof createNote>>, TError = ErrorType<unknown>,
-    TContext = unknown>(options?: { mutation?:UseMutationOptions<TData, TError,{data: BodyType<CreateNoteBody>}, TContext>, request?: SecondParameter<typeof customInstance>}
-): UseMutationResult<
-        TData,
-        TError,
-        {data: BodyType<CreateNoteBody>},
-        TContext
-      > => {
+export const useCreateNote = <
+  TData = Awaited<ReturnType<typeof createNote>>,
+  TError = ErrorType<unknown>,
+  TContext = unknown,
+>(options?: {
+  mutation?: UseMutationOptions<
+    TData,
+    TError,
+    { data: BodyType<CreateNoteBody> },
+    TContext
+  >
+  request?: SecondParameter<typeof customInstance>
+}): UseMutationResult<
+  TData,
+  TError,
+  { data: BodyType<CreateNoteBody> },
+  TContext
+> => {
+  const mutationOptions = getCreateNoteMutationOptions(options)
 
-      const mutationOptions = getCreateNoteMutationOptions(options);
-
-      return useMutation(mutationOptions);
-    }
-    
+  return useMutation(mutationOptions)
+}
